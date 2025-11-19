66ade819dc564ce4162c8237bb14265db752b633
victorjoao820 <joaovictorsov820@gmail.com>
2025-11-18 18:31:18 -0800

    the sameCount cell of containers table are removed and after done  migrate

---
+++ prisma/migrations/20251119022937_dev/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sameCount` on the `containers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."containers" DROP COLUMN "sameCount";
